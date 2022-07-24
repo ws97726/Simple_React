de959bb784634b65f593e16a65293841b8bb6281
Json <1103562550@qq.com>
2022-07-24 16:06:39 +0800

    first  webpack

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '@/global.scss';
+import '@/global';
+
+const App = () => {
+  return <div>hello world</div>;
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
